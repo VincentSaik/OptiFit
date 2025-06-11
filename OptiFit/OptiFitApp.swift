991cc9c4c77f0bdb4e9f485fecc1052eb01f1045
vincentsaikali <vincentsaikali@gmail.com>
2025-06-10 21:30:09 -0400

    Initial Commit

---
+++ OptiFit/OptiFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptiFitApp.swift
+//  OptiFit
+//
+//  Created by Vincent Saikali on 2025-06-10.
+//
+
+import SwiftUI
+
+@main
+struct OptiFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OptiFitTests/OptiFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  OptiFitTests.swift
+//  OptiFitTests
+//
+//  Created by Vincent Saikali on 2025-06-10.
+//
+
+import Testing
+@testable import OptiFit
+
+struct OptiFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
